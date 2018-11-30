8089a56a15fbf1f792fa6e4955e8b5ea2a0a56fa
MariusMM2 <dmunteanu91@yahoo.com>
2018-11-30 12:47:01 +0100

    inital commit

---
+++ src/main/java/com/exam/medilink/MedilinkApplication.java
@@ -0,0 +1,12 @@
+package com.exam.medilink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedilinkApplication.class, args);
+    }
+}
