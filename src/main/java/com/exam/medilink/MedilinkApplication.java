602a53380f881eb45b4088cb8e779913cba1ed2d
panaitescu-paul <panaitescupaul@yahoo.com>
2018-11-30 12:58:11 +0100

    comment

--- src/main/java/com/exam/medilink/MedilinkApplication.java
+++ src/main/java/com/exam/medilink/MedilinkApplication.java
@@ -9,5 +9,6 @@ public class MedilinkApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedilinkApplication.class, args);
 //        comment
+//        comment
     }
 }
