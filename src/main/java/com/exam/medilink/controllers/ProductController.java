a242462d8d358685c2dcd4b04e633159a7241107
panaitescu-paul <panaitescupaul@yahoo.com>
2018-12-03 22:36:31 +0100

    Products - Controller - Template - CSS styling

--- src/main/java/com/exam/medilink/controllers/ProductController.java
+++ src/main/java/com/exam/medilink/controllers/ProductController.java
@@ -1,4 +1,17 @@
 package com.exam.medilink.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductController {
+    @GetMapping("/products")
+    public String products() {
+        return "products";
+    }
+
+    @GetMapping("/product")
+    public String product() {
+        return "product";
+    }
 }
