72d888953427ffe00434272a420c821a65b92111
MariusMM2 <dmunteanu91@yahoo.com>
2018-12-12 14:46:58 +0100

    updated comments

--- src/main/java/com/exam/medilink/repositories/CrudRepository.java
+++ src/main/java/com/exam/medilink/repositories/CrudRepository.java
@@ -4,21 +4,20 @@ import java.util.List;
 
 public interface CrudRepository<T> {
 
-    //Adds a new element in the table
-    //and returns its database id
+    //Adds a new item to the repository and returns its id
     int create(T item);
 
-    //Reads all the elements of the table
+    //Reads all the items in the repository
     List<T> readAll();
 
-    //Reads a specific element from the table
+    //Reads a specific item from the repository
     T read(int id);
 
-    //Updates an existing element in the table
+    //Updates an existing item in the repository
     //returns true upon success, false otherwise
     boolean update(T item);
 
-    //Deletes an element from the table
+    //Deletes an item in the repository
     //returns true upon success, false otherwise
     boolean delete(int id);
 }
